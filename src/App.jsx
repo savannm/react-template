71ffe92495cccf43a3b124492720db6fa4b23a61
Savann Mao <savannmao@Savanns-MacBook-Air.local>
2026-04-03 11:37:02 +1100

    upload all

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="container">
+      <div className="glow"></div>
+      <h1>Hello World</h1>
+      <p>A minimalist React + Vite Template</p>
+    </div>
+  )
+}
+
+export default App
