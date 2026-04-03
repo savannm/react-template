347e877f483e9dc82367447182ff9459da229208
Savann Mao <savannmao@Savanns-MacBook-Air.local>
2026-04-03 17:32:41 +1100

    upload all

--- src/App.jsx
+++ src/App.jsx
@@ -12,5 +12,7 @@ function App() {
   )
 }
 
+export default App;
+
 
 
--- src/components/typescript.tsx
+++ src/components/typescript.tsx
@@ -1,11 +1,53 @@
-function TypeDemo() {
+function Demo() {
+    //First, setup Types
+    //array
+    type typeDrink = string[];
+    //object
+    type typeMenu = {
+        name: string
+        price: number
+        stock: number
+    }
+    type typeDrinkQueue = [{
+        Menu: typeMenu,
+        id: number,
+        status: string,
+    }]
+
+    //main code
+    const drinkName: typeDrink = ["beer", "wine", "cider"];
+    let drinkOrder: typeMenu = {
+        name: "2",
+        price: 10,
+        stock: 1,
+    }
+    let drinkQueue: typeDrinkQueue = [{
+        Menu: {
+            name: "beer",
+            price: 10,
+            stock: 1,
+        },
+        id: 1,
+        status: "ordered",
+    }]
+
     return (
         <div>
             <h1>Type Demo</h1>
-        </div>
+            <p></p>
+            <p>{drinkQueue[0].Menu.name}</p>
+            <button onClick={() => {
+                //adds new drink to queue
+                drinkQueue.push({
+                    Menu: { name: "beer", price: 10, stock: 1 }, id: 2, status: "ordered"
+                })
+            }}>submit</button>
+        </div >
     )
 }
 
-export default TypeDemo; {
-
+export default function TypeDemo() {
+    return (
+        < Demo />
+    )
 }
