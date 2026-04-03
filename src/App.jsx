0ec503065950ccecf1abf1a669ad1e4d6a171a4e
Savann Mao <savannmao@Savanns-MacBook-Air.local>
2026-04-03 11:42:22 +1100

    upload all

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="container">
       <div className="glow"></div>
-      <h1>Hello World</h1>
+      <h1>Hello Sav</h1>
       <p>A minimalist React + Vite Template</p>
     </div>
   )
